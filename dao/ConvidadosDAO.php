bc6c8011093e53923cf30571326463a706536728
lucromanha <romanhalucas@gmail.com>
2025-10-09 20:49:42 -0300

    2025-10-09 20:49

--- dao/ConvidadosDAO.php
+++ dao/ConvidadosDAO.php
@@ -1,6 +1,8 @@
 <?php
 require $_SERVER['DOCUMENT_ROOT']."/html/sistema/obj/Convidados.php";
 
+use Ramsey\Uuid\Uuid;
+
 class ConvidadosDAO{
     private static function estruturarSQL($conn, $ln){
         try{
@@ -26,7 +28,7 @@ class ConvidadosDAO{
     public static function selectIndex($array){
         try{ 
             //$conn = !empty($array['conn']) ? $array['conn'] : throw new Exception('A conexão não foi aberta!');            
-            $conn = self::verExpection(!empty($array['conn']), $array['conn'], 'A conexão não foi aberta!');
+            $conn = verExpection(!empty($array['conn']), $array['conn'], 'A conexão não foi aberta!');
             $id = !empty($array['id']) ? $array['id'] : null;
             
             $sql = $conn->prepare("SELECT * FROM convidados obj WHERE obj.id = :id");
@@ -55,7 +57,7 @@ class ConvidadosDAO{
     ////////////////////////
     public static function selectAll($array){
         try{ 
-            $conn = self::verExpection(!empty($array['conn']), $array['conn'], 'A conexão não foi aberta!');
+            $conn = verExpection(!empty($array['conn']), $array['conn'], 'A conexão não foi aberta!');
             
             $sql = $conn->prepare("SELECT * FROM convidados obj");
             
@@ -72,6 +74,42 @@ class ConvidadosDAO{
             return $objs;
             
         } catch (Exception $ex) {
+            echo "ERRO: {$ex->getMessage()}";
+            return false;
+        }
+    }
+    
+    ///////////////////////////////////////////////
+    //salvar
+    ////////////////////////////////////////////////
+    public static function save($array){
+        try{            
+            $conn = verExpection(!empty($array['conn']), $array['conn'], 'A conexão não foi aberta!');
+            $obj = verExpection(!empty($array['obj']), $array['obj'], 'O objeto não existe!');
+            
+             //id nome celular confirmado dtExpiracao vistoPorUltimo
+            if($obj->getId()>0){
+                //ATUALIZAR
+                $sql = "UPDATE convidados SET";
+                $sql = $conn->prepare($sql);
+                $sql->bindValue(":id", $obj->getId());
+            }else{
+                //CRIAR
+                $sql = "INSERT INTO convidados() "
+                        . "VALUES ()";
+                $sql = $conn->prepare($sql);
+            }
+            
+            $sql->bindValue(":nome", $obj->getNome());//RELACIONAR OBJETO COM PARAMETROS DO BANCO DE DADOS       
+            
+            if($sql->execute()){
+                $id = empty($obj->getID()) ? $conn->lastInsertId() : $obj->getId();
+                return $id;
+            }else{
+                return false;
+            }            
+            
+        } catch (Exception $ex) {            
             echo "ERRO: {$ex->getMessage()}";
             return false;
         }
