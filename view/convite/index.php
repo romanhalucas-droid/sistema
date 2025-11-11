69550523eaa25e072cde4a3d2d16dcc7e8eb1482
lucromanha <romanhalucas@gmail.com>
2025-11-11 19:11:49 -0300

    2025-11-11

--- view/convite/index.php
+++ view/convite/index.php
@@ -64,8 +64,66 @@ if (!empty($id)){
                                 em contato conosco.
                             </p>
                         
-                        <?php elseif ($convidado->getConfirmado()<>1): ?>
+                        <?php elseif ($timeHoje <= $timeDtExp OR $convidado->getConfirmado()<>1): ?>
                             
+                            <!-- mensagem de boas vindas principal -->
+                            <p class="card-text">
+                                Será uma grande alegria contar com sua presença neste momento tão especial,
+                                celebrando a nossa confraternização de final de ano.
+                            </p>
+                            
+                            
+                            <!-- local da festa -->
+                            <p class="card-text">
+                                <strong>Local da cerimônia: </strong>
+                                <br>
+                                <a href="https://maps.app.goo.gl/w85uoMyjpTB91u5V9" target="_blank" 
+                                   style="text-decoration: underline; color: #007BFF;">
+                                    SENAC (Colatina)
+                                    <br>
+                                    Av. Adauto Barcelos de Carvalho, 400, Esplanada, Colatina/ES
+                                </a>
+                            </p>
+                            
+                            <!-- hora da festa -->
+                            <p class="card-text"><strong>Data e hora:</strong> 12 de Dezembro de 2025 - 19 horas</p>
+                            
+                            <!-- mensagem para quem já confirmou a presença (confirmado==3) -->
+                            <?php if($convidado->getConfirmado()==3): ?>
+                                <p class="card-text">
+                                    A sua presença já foi confirmada! <3
+                                    <br>
+                                    Caso tenha ocorrido algum engano ou deseje alterar a sua resposta,
+                                    por favor, entre em contato conosco.
+                                </p>
+                            <?php endif ?>
+                            
+                            <!--CASO ESTEJA NO PRAZO E AINDA NÃO RESPONDEU (CONFIRMADO==1) -->                            
+                            <?php if($timeHoje <= $timeDtExp AND $convidado->getConfirmado()==1): ?>
+                                <p class="mt-4" style="font-size: 1.1rem;">
+                                    Por favor, confirme sua presença clicando em umas das opções abaixo                                                                                   
+                                    até o dia <strong><?= dtSqlToBrasil( $convidado->getDtExpiracao() ) ?>.</strong>
+                                </p>
+                                
+                                <div class="mt-3 bg-danger-subtle gap-2 p-3 rounded-3">
+                                    <!-- BOTAO DE CONFIRMAR PRESENÇA -->
+                                    <a href="/html/sistema/validacao/convite/confirmar_convite.php?id=<?=$convidado->getId()?>&confirmado=3"
+                                       class="btn btn-success mt-1 btn-lg"
+                                    >
+                                        Sim, estarei presente!
+                                    </a>
+                                    
+                                    <!-- BOTAO PARA NEGAR A PRESENÇA (NÃO VAI) -->
+                                    <a href="/html/sistema/validacao/convite/confirmar_convite.php?id=<?=$convidado->getId()?>&confirmado=2"
+                                       class="btn btn-danger mt-1 btn-lg"
+                                    >
+                                        Infelizmente não poderei comparecer.
+                                    </a>
+                                </div>    
+                                
+                            <?php endif ?>
+                                
+                                
                         <?php endif ?>
                     </div>
                 </div>
@@ -73,5 +131,6 @@ if (!empty($id)){
                 <center><h4>Convidado não encontrado!</h4></center>
             <?php endif ?>
         </div>
+        <?php $_SERVER['DOCUMENT_ROOT']."/html/sistema/util/estrutura/rodape.php" ?>
     </body>
 </html>
